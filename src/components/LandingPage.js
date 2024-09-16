e162618bbf53eb528b8de767c8620064c1627a90
ChumanY <cuerpoespin_2000@hotmail.com>
2024-09-16 17:52:10 -0500

    update text

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,59 +5,73 @@ import Logo from "../assets/logo.png";
 import LogoFooter from "../assets/Group.png";
 
 function LandingPage() {
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="page-container">
       <header className="header">
         <div className="header-content">
           <img src={Logo} alt="Logo" className="header-logo" />
           <div className="header-links">
-            <a href="/about">About us</a>
-            <a href="/community">Community</a>
-            <a href="/events">Events</a>
-            <a href="/contact">Contacto</a>
+            <a href="#hero" onClick={() => scrollToSection("hero")}>
+              Inicio
+            </a>
+            <a href="#about" onClick={() => scrollToSection("about")}>
+              Nosotros
+            </a>
+            <a href="#community" onClick={() => scrollToSection("community")}>
+              Galería
+            </a>
           </div>
         </div>
       </header>{" "}
       {/* Nueva Sección con fondo de imagen y otra imagen superpuesta */}
-      <section className="hero-section">
+      <section id="hero" className="hero-section">
         <div className="hero-background">
           <img src={BackgroundFirstSection} />
         </div>
         <div className="hero-content">
-          <h1>Join the Cosmic Crusade Adventure!</h1>
+          <h1>¡Únete a la aventura de Cosmic Crusade!</h1>
           <p>
-            Experience the thrill of Starcraft II inspired gameplay. <br></br>
-            Connect with fellow gamers, share strategies, and become a part of
-            our vibrant community.
+            Experimenta la emoción del juego inspirado en Starcraft II.
+            <br></br>
+            Conéctate con otros jugadores, comparte estrategias y conviértete en
+            parte de nuestra vibrante comunidad.
           </p>
-          <button className="play-button">Play now</button>
+          <button className="play-button">Juega ahora</button>
         </div>
         <div className="hero-image">
           <img src={LogoFooter} alt="Foreground" />
         </div>
       </section>
       {/* Sección Similar a la Imagen Proporcionada */}
-      <section className="image-text-section">
+      <section id="about" className="image-text-section">
         <div className="image-container"></div>
         <div className="text-container">
-          <h2>Título de esta sección</h2>
+          <h2>¿Quiénes somos?</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Somos un grupo de estudiantes de la Universidad Peruana de Ciencias
+            Aplicadas apasionados por los videojuegos y que se encuentran
+            cursando los últimos ciclos de las carreras de Ingeniería de
+            Software y Ciencias de la Computación. Deseosos por revivir la
+            experiencia de Starcraft y empapados de nostalgia, los miembros del
+            proyecto decidieron hace un videojuego tributo a la aclamada
+            franquicia de Blizzard, naciendo así Cosmic Crusade. ¡Esperamos seas
+            parte de este extraordinario viaje con nosotros!
           </p>
         </div>
       </section>
       {/* Community Showcase Section */}
-      <section className="community-showcase">
-        <h2>Community Showcase</h2>
+      <section id="community" className="community-showcase">
+        <h2>Galería multimedia</h2>
         <p>
-          Explore our vibrant community through stunning visuals that capture
-          the essence of gameplay and events.
+          Explora nuestra vibrante comunidad a través de impresionantes imágenes
+          que capturan la esencia del juego.
         </p>
         <div className="image-grid">
           <div className="image-card image1"></div>
@@ -74,16 +88,21 @@ function LandingPage() {
         <div className="footer-content">
           <img src={Logo} alt="Logo" className="footer-logo" />
           <div className="footer-links">
-            <a href="/about">About us</a>
-            <a href="/community">Community</a>
-            <a href="/events">Events</a>
-            <a href="/contact">Contacto</a>
+            <a href="#hero" onClick={() => scrollToSection("hero")}>
+              Inicio
+            </a>
+            <a href="#about" onClick={() => scrollToSection("about")}>
+              Nosotros
+            </a>
+            <a href="#community" onClick={() => scrollToSection("community")}>
+              Galería
+            </a>
           </div>
         </div>
         <p className="footer-rights">
-          © 2024 Cosmic Crusade. All rights reserved. Join the Cosmic Crusade
-          community and experience the thrilling gameplay of this Starcraft II
-          inspired creation.
+          © 2024 Cosmic Crusade. Reservados todos los derechos. Únase a la
+          comunidad Cosmic Crusade y experimente la emocionante jugabilidad de
+          esta creación inspirada en Starcraft II.
         </p>
       </footer>
     </div>
